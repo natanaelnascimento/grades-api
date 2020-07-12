b270a1972f90202553242bc2a05c4d608f8aa8ec
Natanael Nascimento <hello@natanael.dev>
2020-07-12 17:21:04 -0300

    Correçao no repository para chamar metodo correto de inclusao

--- repositories/grades.js
+++ repositories/grades.js
@@ -3,7 +3,7 @@ import clone from '../helpers/clone.js';
 
 const create = async (grade) => {
     const model = new gradeModel(grade);
-    const created = await gradeModel.save(model);
+    const created = await gradeModel.create(model);
     created.id = created._id;
     return created;
 }
